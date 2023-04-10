1a8158191184d13b5c914f0bf46286e439ae2066
zahra987 <zhr.ramezanpour@gmail.com>
2023-04-10 13:06:53 +0430

    The submenu is under construction

---
+++ home/home.js
@@ -0,0 +1,23 @@
+var mainNavList = document.querySelectorAll('header .main-nav ul li');
+var mainNavSubmenu = document.querySelector('header .submenu');
+
+mainNavList[0].onmousemove=function () {
+  mainNavSubmenu.style.visibility='visible';
+}
+mainNavList[0].onmouseout=function () {
+  mainNavSubmenu.style.visibility='hidden';
+}
+
+mainNavList[1].onmousemove=function () {
+  mainNavSubmenu.style.visibility='visible';
+}
+mainNavList[1].onmouseout=function () {
+  mainNavSubmenu.style.visibility='hidden';
+}
+
+mainNavList[4].onmousemove=function () {
+  mainNavSubmenu.style.visibility='visible';
+}
+mainNavList[4].onmouseout=function () {
+  mainNavSubmenu.style.visibility='hidden';
+}

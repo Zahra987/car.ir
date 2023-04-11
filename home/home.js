597898b66cc1ae75f3d3414780d6735ff55afeca
zahra987 <zhr.ramezanpour@gmail.com>
2023-04-11 17:23:53 +0430

    The existing sub-menu in html was removed and created with javascript

--- home/home.js
+++ home/home.js
@@ -1,30 +1,38 @@
 var mainNavList = document.querySelectorAll('header .main-nav ul li');
-var mainNavSubmenu = document.querySelector('header .submenu');
+var header = document.getElementById('header-container');
+var submenu=document.createElement('div');
 
 mainNavList[0].onmousemove=function () {
-  mainNavSubmenu.style.visibility='visible';
+  navItemHover();
 }
 mainNavList[0].onmouseout=function () {
-  mainNavSubmenu.style.visibility='hidden';
+  submenu.style.visibility='hidden';
 }
-
 mainNavList[1].onmousemove=function () {
-  mainNavSubmenu.style.visibility='visible';
+  navItemHover();
 }
 mainNavList[1].onmouseout=function () {
-  mainNavSubmenu.style.visibility='hidden';
+  submenu.style.visibility='hidden';
 }
-
 mainNavList[4].onmousemove=function () {
-  mainNavSubmenu.style.visibility='visible';
+  navItemHover();
 }
 mainNavList[4].onmouseout=function () {
-  mainNavSubmenu.style.visibility='hidden';
+  submenu.style.visibility='hidden';
 }
-////
-mainNavSubmenu.onmousemove=function () {
-  mainNavSubmenu.style.visibility='visible';
+function navItemHover() {
+header.after(submenu);
+submenu.innerHTML="asdfghjkl";
+submenu.style.width='100vw';
+submenu.style.height='70vh';
+submenu.style.backgroundColor='red';
+submenu.style.top='135px';
+submenu.style.visibility='visible';
+submenu.style.transition='visibility 0.4s';
+submenu.onmouseover=function () {
+  submenu.style.visibility='visible';
 }
-mainNavSubmenu.onmouseout=function () {
-  mainNavSubmenu.style.visibility='hidden';
+submenu.onmouseout=function () {
+  submenu.style.visibility='hidden';
+}
 }
